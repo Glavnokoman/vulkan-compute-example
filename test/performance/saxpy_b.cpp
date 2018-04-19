bb2d15d82a9d6603a7b14072d30bb66ce6e652b5
Svyatoslav Savenko <svyatoslav.savenko@leica-geosystems.com>
2018-04-19 09:42:53 +0200

    wip

--- test/performance/saxpy_b.cpp
+++ test/performance/saxpy_b.cpp
@@ -3,20 +3,22 @@
 #include <example_filter.h>
 #include <vulkan_helpers.hpp>
 
-static void BM_StringCreation(benchmark::State& state) {
+namespace {
 
-  for (auto _ : state)
-    std::string empty_string;
+   void BM_StringCreation(benchmark::State& state) {
+     for (auto _ : state)
+       std::string empty_string;
+   }
+
+   // Define another benchmark
+   void BM_StringCopy(benchmark::State& state) {
+     std::string x = "hello";
+     for (auto _ : state)
+       std::string copy(x);
+   }
 }
-// Register the function as a benchmark
+
 BENCHMARK(BM_StringCreation);
-
-// Define another benchmark
-static void BM_StringCopy(benchmark::State& state) {
-  std::string x = "hello";
-  for (auto _ : state)
-    std::string copy(x);
-}
 BENCHMARK(BM_StringCopy);
 
 BENCHMARK_MAIN();
